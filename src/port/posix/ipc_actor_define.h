a525717241b5b2cbdc375d1564f6afa20d4d5abf
BuriKizilkaya <burak.kizilkaya@outlook.com>
2026-07-22 13:16:39 +0000

    Add common IPC actor registration functions and update port-specific headers

--- src/port/posix/ipc_actor_define.h
+++ src/port/posix/ipc_actor_define.h
@@ -7,29 +7,10 @@
  */
 #pragma once
 
+#include "../../ipc_actor_define_common.h"
 #include "ipc_port_state.h"
 
 struct ipc_actor;
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-void _ipc_actor_register_static(struct ipc_actor *actor);
-void _ipc_actor_register_handler_static(struct ipc_actor *actor, ipc_msg_desc_t *desc,
-                                        ipc_actor_msg_handler_t handler);
-void _ipc_actor_register_start_hook_static(struct ipc_actor *actor,
-                                           ipc_actor_lifecycle_hook_t hook);
-void _ipc_actor_register_stop_hook_static(struct ipc_actor *actor, ipc_actor_lifecycle_hook_t hook);
-void _ipc_actor_register_unknown_hook_static(struct ipc_actor *actor,
-                                             ipc_actor_unknown_handler_t hook);
-void _ipc_actor_register_supervision_static(struct ipc_actor *actor,
-                                            ipc_supervision_strategy_t strategy);
-void _ipc_actor_register_failure_hook_static(struct ipc_actor *actor,
-                                             ipc_actor_failure_hook_t hook);
-void ipc_dispatch_actor_handlers(struct ipc_actor *self, const struct ipc_msg *msg);
-#ifdef __cplusplus
-}
-#endif
 
 /* Parameter names intentionally avoid struct field names such as
  * stack_size/priority/queue_depth. Preprocessor substitution is purely
--- src/port/zephyr/ipc_actor_define.h
+++ src/port/zephyr/ipc_actor_define.h
@@ -9,6 +9,7 @@
  */
 #pragma once
 
+#include "../../ipc_actor_define_common.h"
 #include "ipc_port_state.h"
 
 #include <zephyr/init.h>
@@ -19,19 +20,6 @@ struct ipc_actor;
 #ifdef __cplusplus
 extern "C" {
 #endif
-void _ipc_actor_register_static(struct ipc_actor *actor);
-void _ipc_actor_register_handler_static(struct ipc_actor *actor, ipc_msg_desc_t *desc,
-                                        ipc_actor_msg_handler_t handler);
-void _ipc_actor_register_start_hook_static(struct ipc_actor *actor,
-                                           ipc_actor_lifecycle_hook_t hook);
-void _ipc_actor_register_stop_hook_static(struct ipc_actor *actor, ipc_actor_lifecycle_hook_t hook);
-void _ipc_actor_register_unknown_hook_static(struct ipc_actor *actor,
-                                             ipc_actor_unknown_handler_t hook);
-void _ipc_actor_register_supervision_static(struct ipc_actor *actor,
-                                            ipc_supervision_strategy_t strategy);
-void _ipc_actor_register_failure_hook_static(struct ipc_actor *actor,
-                                             ipc_actor_failure_hook_t hook);
-void ipc_dispatch_actor_handlers(struct ipc_actor *self, const struct ipc_msg *msg);
 int ipc_port_register_static_actor_resources(struct ipc_actor *actor, void *stack,
                                              size_t stack_size, char *msgq_buf, size_t queue_depth);
 #ifdef __cplusplus
